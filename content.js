0d7a5ec846cf60261e04c8717b177ec14f7bfbf1
lukehcjackson <luke.hc.jackson@gmail.com>
2023-12-08 14:57:30 +0000

    Wait for title to load before trying to display it
    Using async/await to wait for title to be properly loaded before it tries to display it, to avoid just displaying an empty string. Also changed content script to run when the page has fully loaded rather than waiting for a click or scroll.

--- content.js
+++ content.js
@@ -1,17 +1,22 @@
-//want to disable generate button NOT IN THIS SCRIPT  so it's disabled even on other pages
+//this script ONLY runs on wikipedia and is injected into the code of the browser
 
-document.addEventListener('click', () => active(), { once: true });
-document.addEventListener('scroll', () => active(), { once: true });
+//wait for page to fully load and then call active()
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+      active();
+    }
+}
 
 function active() {
     console.log("Loading wikipedia page title");
     //APPEARS IN BROWSER CONSOLE!!
 
+    //get wikipedia page title
     title = document.getElementsByClassName("mw-page-title-main")[0].innerHTML;
 
     console.log(title);
 
-    //store the title
+    //store the title in local chrome storage
     chrome.storage.local.set({"pageTitle": title});
 
 }
--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+//this script runs alongside the popup and is essentially in it's own contained universe, separate from the browser
+
 function disableButton() {
     document.getElementById("runAIbutton").disabled = true;
 }
@@ -6,43 +8,48 @@ function enableButton() {
     document.getElementById("runAIbutton").disabled = false;
 }
 
-function hello() {
-    alert('hello');
-    disableButton();
-    /*
-    chrome.tabs.executeScript({
-      file: 'alert.js'
-    }); 
-    */
+const getTitleFromStorage = async (key) => {
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get([key], function (result) {
+        if (result[key] === undefined) {
+          reject();
+        } else {
+          resolve(result[key]);
+        }
+      });
+    });
+  };
+
+async function loadTitle() {
+    let title = await getTitleFromStorage('pageTitle');
+    createTitleText(title);
 }
 
-window.onload = disableButton();
-
-//want: WAIT for "pageTitle" to be set BEFORE we can enable the button or display it's content
-
-//want: this to happen on opening the popup, not clicking on it
-//so: page loads -> user clicks/scrolls? -> store title -> user opens popup -> enable button, display title
-//      |                   |                   |                   ^
-//      V                   V                   V                   |
-//         ---------------------------->do not enable button or show title until
-
-document.addEventListener('click', () => loadTitle(), { once: true });
-
-function loadTitle() {
-    title = ""
-    chrome.storage.local.get(("pageTitle"),function(res) {
-        console.log(res["pageTitle"]);
-        title = res["pageTitle"];  
-    });
+function createTitleText(title) {
+    console.log("title:");
+    console.log(title);
     enableButton();
     const newDiv = document.createElement("div");
     const divText = document.createTextNode(title);
     newDiv.appendChild(divText);
     document.body.appendChild(newDiv);
-    
 }
 
-document.getElementById("runAIbutton").addEventListener('click', hello);
+window.onload = disableButton();
+
+//want: WAIT for "pageTitle" to be set BEFORE we can enable the button or display it's content
+//want: loadtitle to happen on opening the popup, not clicking on it
+
+//so: page loads -> user clicks/scrolls? -> store title -> user opens popup -> loadTitle(), enable button, display title
+//      |                   |                   |                   ^
+//      V                   V                   V                   |
+//    --X-------------------X---->do not enable button or show title until title stored and popup opened
+
+document.addEventListener('click', () => loadTitle(), { once: true });
+
+//not working because every click both loads the title, creates a div, and disables the button
+//but then why don't multiple text elements appear?
+document.getElementById("runAIbutton").addEventListener('click', disableButton());
 
 
 
