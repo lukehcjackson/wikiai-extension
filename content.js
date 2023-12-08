c80fe0a8e3bb37c17f08078c672679a1eb609a29
lukehcjackson <luke.hc.jackson@gmail.com>
2023-12-08 00:23:38 +0000

    Finds Wikipedia page title when tab is active
    Checks for a click or a scroll (or both) before actually doing anything. These eventlisteners only fire once. Once they go off, call a function which finds the title by classname and getting it's inner html.

--- content.js
+++ content.js
@@ -1,3 +1,12 @@
-//document.addEventListener('click', () => alert('Click occurred!'));
-//document.addEventListener('scroll', () => alert('Scroll occurred!'));
+document.addEventListener('click', () => active(), { once: true });
+document.addEventListener('scroll', () => active(), { once: true });
 
+function active() {
+    console.log("Loading wikipedia page title");
+    //APPEARS IN BROWSER CONSOLE!!
+
+    title = document.getElementsByClassName("mw-page-title-main")[0].innerHTML;
+
+    console.log(title);
+
+}
